43e8192e2a28a1d585131acadb86b1977c57f725
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-03-13 13:03:32 +0100

    modularized functions with isGeo and isArith functions

--- app/library.js
+++ app/library.js
@@ -3,25 +3,31 @@
 module.exports = {
 
   aritGeo: function(arr) {
-  	 var arthCount=0;
-     var geoCount=0;
-  	 if(arr.length === 0){
-        return 0;
-      }
+    var arthCount = 0;
+    var geoCount = 0;
+    if (arr.length === 0) {
+      return 0;
+    }
+    var i;
 
-        for (var i=0;i<arr.length-2;i++){
-             if(arr[i+1]-arr[i]===arr[i+2]-arr[i+1]){
-                 arthCount++;
-            }
-            else if(arr[i+1]/arr[i] ===arr[i+2]/arr[i+1]){
-                 geoCount++;
-            }
-            else return -1;
-        }
-         if(arthCount===arr.length-2)
-             return "Arithmetic";
-         else if(geoCount===arr.length-2)
-             return "Geometric";
+    function isArith(i) {
+      return (arr[i + 1] - arr[i] === arr[i + 2] - arr[i + 1])
+    }
+
+    function isGeo(i) {
+      return (arr[i + 1] / arr[i] === arr[i + 2] / arr[i + 1])
+    }
+
+    for (i = 0; i < arr.length - 2; i++) {
+      if (isArith(i)) {
+        arthCount++;
+      } else if (isGeo(i)) {
+        geoCount++;
+      } else return -1;
+    }
+    if (arthCount === arr.length - 2)
+      return "Arithmetic";
+    else if (geoCount === arr.length - 2)
+      return "Geometric";
   }
-
 }
